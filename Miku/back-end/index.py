f5b22d365b6b92ae9780c2e8dab18d2ad0fd2b67
JuanBr17 <juansp.br17@gmail.com>
2025-06-09 10:57:14 -0500

    Add files via upload

--- Miku/back-end/index.py
+++ Miku/back-end/index.py
@@ -1,20 +1,43 @@
 from flask import Flask, jsonify, request, render_template, render_template_string
+import pymysql
 
 app=Flask(__name__)
 
-@app.route("/")
+def conectar(vhost, vuser, vpass, vdb):
+    conn= pymysql.connect(host=vhost, user=vuser, passwd=vpass, db= vdb)
+    return conn
+
+
+@app.route("/", methods=['GET'])
 def inicio():
     return render_template("index.html")
 
-@app.route("/inicio, methods=['POST']")
-def formulario():
-    if formulario == 'POST':
-        user = request.form.get['email']
+
+@app.route("/registrar", methods=['GET','POST'])
+def add():
+    try:
+        if request.method == 'POST':
+            usuario = request.form.get('email')
+            contrasena = request.form.get('passw')
+            print(usuario)
+            print(contrasena)
         
-        return render_template_string('''
-                <script>alert("Bienvenido, {{ user }}!");</script>
-                <a href="/">Volver</a>
-            ''', user=user)
+        
+            conn=conectar('localhost', 'root', '1711', 'usuarios')    
+            cur = conn.cursor()
+            cur.execute("INSERT into usuarios (usuario, contrasena) VALUES (%s, %s)", (usuario, contrasena))
+            conn.commit()
+            cur.close()
+            conn.close()
+        else:
+            print("hola")
+            
+        return  render_template("prueba.html")
+    except Exception as error:
+        print(error)
+        
+        return render_template("prueba.html")
+
         
 
 if __name__=='__main__':
